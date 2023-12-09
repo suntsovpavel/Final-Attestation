fb1bc131e16603def4263602a143474c37904517
suntsovpavel <suntsovpavel@mail.ru>
2023-12-09 22:48:41 +0300

    working classes Pet, PackAnimal

--- src/RegisterAnimals/model/HumanFriends/PackAnimal.java
+++ src/RegisterAnimals/model/HumanFriends/PackAnimal.java
@@ -1,5 +1,30 @@
 package RegisterAnimals.model.HumanFriends;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 //класс вьючного животного
-public class PackAnimal extends Pet{
+public class PackAnimal extends Pet {
+    private List<String> packingCommands;   //добавляем опционные команды нагрузить - разгрузить
+
+    public PackAnimal(String name, String type, Date dateBirth, ArrayList<String> commands) {
+        super(name, type, dateBirth, commands);
+        packingCommands = new ArrayList<>();
+        packingCommands.add("load"); //добавляем 2 команды "нагрузить - разгрузить"
+        packingCommands.add("unload");
+    }
+
+    public List<String> getPackingCommands(){
+        return packingCommands;
+    }
+
+    @Override
+    //Возвращаем команды с учетом дополнительных: packingCommands
+    public List<String> getCommands() {
+        List<String> list =super.getCommands();
+        for (String one : packingCommands)
+            list.add(one);
+        return list;
+    }
 }
--- src/RegisterAnimals/model/HumanFriends/Pet.java
+++ src/RegisterAnimals/model/HumanFriends/Pet.java
@@ -1,21 +1,39 @@
 package RegisterAnimals.model.HumanFriends;
 
-import RegisterAnimals.model.group.Group;
-import saveload.LoadFrom;
-import saveload.SaveTo;
-
+import java.util.List;
 import java.util.Date;
 
 public class Pet {
     private String name;
     private String type;
     private Date dateBirth;
-    public Pet(String name, String type, ){
+    private List<String> commands;
 
+    public Pet(String name, String type, Date dateBirth, List<String> commands){
         this.name = name;
         this.type = type;
-        this.loader = loader;
-        this.informerHuman = new ReportablePrize();
-        this.errorMessage = null;
+        this.dateBirth = dateBirth;
+        this.commands = commands;
+    }
+
+    public Date getDateBirth() {
+        return dateBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public List<String> getCommands() {
+        return commands;
+    }
+    //Добавить возможность обучать животных новым командам
+    public void addCommand(String command){
+        if(!commands.contains(command))
+            commands.add(command);
     }
 }
